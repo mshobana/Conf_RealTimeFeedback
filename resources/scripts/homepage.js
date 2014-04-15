1b2a12645e67e1b6b398b1f2a5a72e2cb2f7e4a7
Shobana <Admin@Shobanas-MacBook.local>
2014-04-15 12:44:28 +0530

    Moved the methods handling server calls to server.js.<Shobana>

--- resources/scripts/homepage.js
+++ resources/scripts/homepage.js
@@ -1,14 +1,3 @@
-var server = io.connect("http://localhost:8000");
-
-server.on('connect', function(data){
-   var userName = prompt("Enter your name please!", null);
-   server.emit("join", userName);
-});
-
-server.on('feedbackByOthers', function(name, feedback){
-   appendFeedback(name, feedback);
-});
-
 RenderTalkLinks();
 
 $('#feedbackSubmit').click(function(){
