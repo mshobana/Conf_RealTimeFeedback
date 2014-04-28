d542297231159e6af94200d370915adaea92498e
Ashok Gowtham M <ashokgo@thoughtworks.com>
2014-04-28 23:14:54 +0530

    Submit when {EnterKey} presed in input box <Ashok>

--- resources/scripts/homepage.js
+++ resources/scripts/homepage.js
@@ -1,8 +1,22 @@
 RenderTalkLinks();
 
 $('#feedbackSubmit').click(function(){
-   var feedback = $('#feedback-text').val();
-   server.emit("feedback", feedback);
-   appendFeedback("Me", feedback);
-   $('#feedback-text').val("");
+    var feedback = $('#feedback-text').val();
+    server.emit("feedback", feedback);
+    appendFeedback("Me", feedback);
+    $('#feedback-text').val("");
+});
+
+$(document).ready(function() {
+    $('#feedback-text').bind('keydown', function(event) {
+        // track enter key
+        var keycode = (event.keyCode || event.which || event.charCode);
+        if (keycode == 13) { // keycode for enter key
+            // force the 'Enter Key' to implicitly click the Enter button
+            $('#feedbackSubmit').click();
+            return false;	//event is handled
+        } else  {
+            return true;
+        }
+    });
 });
